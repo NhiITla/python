a46c75e9c2d82eb96efa96925eea8a3d65cb885e
Nhi Nguyen <nhiitctu@gmail.com>
2024-08-25 18:13:13 +0700

    Added Unitest with Django

---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+"""Sample tests"""
+
+from django.test import SimpleTestCase # type: ignore
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+    def test_add_numbers(self):
+        """Test adding number together."""
+        res=calc.add(5,6)
+        self.assertEqual(res,12)
